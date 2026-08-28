7e27289a82c8ffeeb4959dd14d759e0f0a647840
stephanie <berandalstephanie490@gmail.com>
2026-08-28 10:19:08 +0800

    Update footer.php

---
+++ output  2/footer.php
@@ -0,0 +1,10 @@
+<footer class="bg-primary text-white text-center py-3 mt-4">
+    <div class="container">
+        <p class="mb-0">
+            &copy; <?php echo date("Y"); ?> Web Systems and Technologies
+        </p>
+    </div>
+</footer>
+
+</body>
+</html>
